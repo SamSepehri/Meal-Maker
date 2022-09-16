5369208e013d4b697e609387534c116e0f8ae232
samisep <sam.sepehri@gmx.de>
2022-09-16 23:47:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meal-Maker
+ Codeacademy project
---
+++ app.js
@@ -0,0 +1,27 @@
+const menu = {
+    _meal: '',
+    _price: 0,
+
+    set meal(mealToCheck) {
+        if (typeof mealToCheck === 'string') {
+            return this._meal = mealToCheck;
+        }
+    },
+    set price(priceToCheck) {
+        if (typeof priceToCheck === 'number') {
+            return this._price = priceToCheck;
+        }
+    },
+
+    get todaysSpecial() {
+        if (this._meal && this._price) {
+            return `Today’s Special is Spaghetti for ${this._price}!'`;
+        } else {
+            return 'Meal or price was not set correctly!';
+        }
+    },
+}
+
+menu.meal = 'string';
+menu.price = 10;
+console.log(menu.todaysSpecial);
